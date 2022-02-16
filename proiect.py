bf489bc47c1dc4ddb451937bc6e087c78640e78e
DennisBunea <dennis@valohai.com>
2022-02-16 21:52:54 +0200

    no message

--- proiect.py
+++ proiect.py
@@ -21,18 +21,12 @@ default_parameters = {
 }
 
 def log_metadata(epoch, logs):
- 
     with valohai.logger() as logger:
- 
         logger.log('epoch', epoch)
- 
         logger.log('accuracy', logs['accuracy'])
- 
         logger.log('loss', logs['loss'])
- 
- 
- 
-input_path = valohai.inputs('dataset').path()
+        
+input_path = valohai.inputs('train').path()
 with np.load(input_path, allow_pickle=True) as f:
     x_train, y_train = f['x_train'], f['y_train']
     x_test, y_test = f['x_test'], f['y_test']
@@ -52,13 +46,18 @@ model.compile(optimizer=optimizer,
             loss=loss_fn,
             metrics=['accuracy'])
  
- 
 callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_metadata)
- 
 model.fit(x_train, y_train, epochs=valohai.parameters('epoch').value, callbacks=[callback])
  
  
-model.evaluate(x_test,  y_test, verbose=2)
+test_loss, test_accuracy = model.evaluate(x_test,  y_test, verbose=2)
+ 
+with valohai.logger() as logger:
+ 
+    logger.log('test_accuracy', test_accuracy)
+ 
+    logger.log('test_loss', test_loss)
+ 
  
 output_path = valohai.outputs().path('model.h5')
 model.save(output_path)
